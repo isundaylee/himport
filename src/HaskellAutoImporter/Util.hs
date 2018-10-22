e2a22003816360e02d1b32e3e32b6ad892060689
Jiahao Li <reg@ljh.me>
2018-10-22 10:43:38 -0400

    Implements importedName

--- src/HaskellAutoImporter/Util.hs
+++ src/HaskellAutoImporter/Util.hs
@@ -1,7 +1,15 @@
 module HaskellAutoImporter.Util
   ( isIdentQualified
+  , importedName
   )
 where
 
+import           Data.List                      ( findIndex )
+
 isIdentQualified :: String -> Bool
 isIdentQualified = not . null . filter (== '.')
+
+importedName :: String -> String
+importedName ident = case (findIndex (== '@') ident) of
+  Nothing -> ident
+  Just i  -> (drop (i + 1) ident)
--- test/HaskellAutoImporter/UtilSpec.hs
+++ test/HaskellAutoImporter/UtilSpec.hs
@@ -13,3 +13,11 @@ spec = do
       (isIdentQualified "Data.List.map") `shouldBe` True
     it "return false with unqualified ident" $ do
       (isIdentQualified "map") `shouldBe` False
+
+  describe "importedName" $ do
+    it "should work in cases with @" $ do
+      (importedName "Data.List.@map") `shouldBe` "map"
+      (importedName "Data.@List.map") `shouldBe` "List.map"
+
+    it "should work in cases without @" $ do
+      (importedName "Data.List.map") `shouldBe` "Data.List.map"
